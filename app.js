96251a78f3355438702cb066e0b11aa2dfbf1309
atikur-rabbi <msawon94@gmail.com>
2021-06-20 11:40:00 +0600

    port

--- app.js
+++ app.js
@@ -3,4 +3,8 @@ var app = express();
 var port = 8080 || process.env.PORT
 app.use('/', express.static('public'));
 
-app.listen(port);
+// app.listen(port);
+
+app.listen(port, function () {
+    console.log(`Listening on port ${port}...`)
+})
